91346e0a6c1e22215ce880cf5d16be94b143ee98
Daniel <65277548+Daniel4-Scratch@users.noreply.github.com>
2020-09-29 10:04:19 +1000

    Update PythonGRAPE.py

--- PythonGRAPE.py
+++ PythonGRAPE.py
@@ -12,12 +12,26 @@ P            G G G G G G
 Version 1.0.0
 -Python Grape-
 A simple python module.
+
+==Example==
+from PythonGRAPE import *
+var = ["banana", "apple"]
+print(read(var, 1))
 ==Credits==
 By Daniel4-Scratch
 
 """
 
-def info(): print("Make sure you import the turtle module.")
+import warnings, random
 
-def square(radius):
+
+# Functions
+def square_turtle(radius):
+  pendown()
+  for i in range(4):
+    forward(radius)
+    left(90)
+  penup()
   
+def read(var, number):
+  return var[number]
